9753dd2027390eb1bc4bf87bfa75f4003d54639c
sinwooKim <rlatlsdn98@naver.com>
2025-12-17 14:05:04 +0900

    chap05 lecture src

---
+++ 03_KIMSINWOO/chap05-cookie-session-lecture-source/src/main/java/com/ohgiraffers/chap05cookiesessionlecturesource/section01/cookie/CookieHandler.java
@@ -0,0 +1,39 @@
+package com.ohgiraffers.chap05cookiesessionlecturesource.section01.cookie;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.*;
+import jakarta.servlet.http.*;
+
+import java.io.IOException;
+
+@WebServlet("/cookie")
+public class CookieHandler extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        /* 전달 받은 데이터(파라미터) 얻어오기 */
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+
+        /* 1. 쿠키 생성 */
+        Cookie cookie1 = new Cookie("firstName",firstName);
+        Cookie cookie2 = new Cookie("lastName",lastName);
+
+        /* 2. 쿠키 만료 시간 */
+        cookie1.setMaxAge(60*60*24); // 초 단위로 입력
+        cookie2.setMaxAge(60*60*24); // (0 입력시 -> 0초 짜리 쿠키로 덮어쓰기 -> 쿠키 삭제)
+
+        /* 3. 응답 헤더에 쿠키를 담아서 클라이언트에게 전달 */
+        response.addCookie(cookie1);
+        response.addCookie(cookie2);
+
+        response.sendRedirect("/cookie-redirect");
+
+
+    }
+}
